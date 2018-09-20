ebd0116ba15478f84ee6450cfa622c59ff8e58cd
syunha <42061968+syunha@users.noreply.github.com>
2018-09-21 02:10:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(document).on('click','header a',function(event){
+	$(document).on('click','header li',function(event){
 		history.pushState(null,null,event.target.href);
 	
 	$('article').load(event.target.href+'.content')
